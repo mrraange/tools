dec994eb7af4f792ee4d349e75de785f69498746
Range <100018176+mrraange@users.noreply.github.com>
2023-01-13 12:10:37 +0300

    Update snapshot_okp4.sh

--- snapshot_okp4.sh
+++ snapshot_okp4.sh
@@ -2,7 +2,6 @@ sudo systemctl stop okp4d
 cp $HOME/.okp4d/data/priv_validator_state.json $HOME/.okp4d/priv_validator_state.json.backup
 rm -rf $HOME/.okp4d/data
 cd $HOME/.okp4d
-wget https://rangenodes.com/snapshots/snapshot_okp4.tar.gz
-tar -xvf snapshot_okp4.tar.gz
+curl -L https://rangenodes.com/snapshots/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.okp4d
 mv $HOME/.okp4d/priv_validator_state.json.backup $HOME/.okp4d/data/priv_validator_state.json
-sudo systemctl restart okp4d && journalctl -u okp4d -f --no-hostname -o cat
+sudo systemctl restart okp4d && sudo journalctl -u okp4d -f --no-hostname -o cat
